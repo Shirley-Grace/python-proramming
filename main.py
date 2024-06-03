56a62d97b356c0a1220706b2eac8d538c67129b0
Shirley_Grace <giray.mungai@gmail.com>
2024-06-03 20:27:09 +0300

    game play rps

---
+++ main.py
@@ -0,0 +1,11 @@
+length = int(input('enter the length ; '))
+width = int(input('enter the width ; '))
+
+area = length * width
+print(area)
+
+pi = 3.14
+rad = int(input('enter the radius ; '))
+
+circle_area = pi * rad * rad
+print(circle_area)
---
+++ math.py
@@ -0,0 +1,8 @@
+import math
+
+# print(math.pi)
+# print (math.e)
+
+x = 81
+print(math.sqrt(x))
+
